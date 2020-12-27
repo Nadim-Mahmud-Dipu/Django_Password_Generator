bc4f95a2009211c147055d311c2a6e2999c189cb
unknown <nadim.dipu@northsouth.edu>
2020-12-27 23:35:11 +0600

    Second commit.

--- djangoProject_Password_Generator/settings.py
+++ djangoProject_Password_Generator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qd-@$pimyr6tp*x@t_^z_*gtiyl+0o)o+)czaid)vt(so0lra$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-password-generator.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
